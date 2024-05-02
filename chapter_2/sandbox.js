719948a6e08cc56dd4ef92d0bb42c8c5896b698a
Tyson La <Tysonala@hotmail.com>
2024-05-02 11:56:38 -0500

    JavaScript Tutroial

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,22 @@
+const age = 25;
+const year = 2019;
+
+console.log("I am",age,"years old")
+
+const name = 'mario';
+constlikes = '10';
+
+let result = `${name} is ${age} years old `
+console.log(result);
+
+let martial_artists = ['chun-li', 'goku', 'vegeta'];
+console.log(martial_artists)
+
+const car = function(){
+
+}
+
+function car(){
+
+}
+
